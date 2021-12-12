072053e8556925353bd1efbdaabba72a58a37b08
CyborgOctopus <pdasedano@gmail.com>
2021-12-12 18:08:55 -0500

    Added a ProgressBar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Question } from '../Question/Question';
 import { NavigationBar } from '../NavigationBar/NavigationBar';
+import {ProgressBar} from "../ProgressBar/ProgressBar";
 
 const App = () => {
   return (
     <div className='App'>
         <h1 className='pageTitle'>What Kind of Culture Mind are You?</h1>
         <div className='displayArea'>
+            <ProgressBar />
             <Question />
             <NavigationBar />
         </div>
---
+++ src/components/ProgressBar/ProgressBar.js
@@ -0,0 +1,10 @@
+import './ProgressBar.css';
+
+export const ProgressBar = () => {
+    return (
+        <div className='ProgressBar'>
+            <div className='rect' />
+            <p>Question 1 of 15</p>
+        </div>
+    );
+};
